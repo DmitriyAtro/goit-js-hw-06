01eed1f92a73ea2d76a4ef2b68303e1295de3279
DmitriyAtro <atroshchenkod@gmail.com>
2022-04-26 19:33:06 +0300

    task-08 - done
    Выполнил задание 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (
+    event.currentTarget.elements.email.value === '' ||
+    event.currentTarget.elements.password.value === ''
+  ) {
+    alert('Заповніть всі поля форми!');
+  } else {
+    const email = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+    const formData = {
+      email,
+      password,
+    };
+    event.currentTarget.reset();
+    console.log(formData);
+  }
+}
