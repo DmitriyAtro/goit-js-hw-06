ba7418a25200fc298f65920927935aa1367f99bf
DmitriyAtro <atroshchenkod@gmail.com>
2022-04-26 19:44:42 +0300

    task-09 - done
    Задание 9 выполнено

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const changeBtnEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+
+changeBtnEl.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+}
