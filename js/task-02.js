fa9651f9dde73c0638dc2e1d16018e95888b8fb5
DmitriyAtro <atroshchenkod@gmail.com>
2022-04-26 17:18:27 +0300

    task-02 - done
    выполнено задание 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listEl = document.querySelector('#ingredients');
+
+const items = ingredients.map(item => {
+  const ingredientItem = document.createElement('li');
+  ingredientItem.textContent = item;
+  ingredientItem.classList.add('item');
+  return ingredientItem;
+});
+
+listEl.append(...items);
